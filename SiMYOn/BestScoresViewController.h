d686f82416e349ae0f0d207e7f3bacca81f22993
Haroldo Gondim <hgtf@cin.ufpe.br>
2015-03-04 21:24:31 -0300

    updating best scores assets and labels

--- SiMYOn/BestScoresViewController.h
+++ SiMYOn/BestScoresViewController.h
@@ -13,12 +13,20 @@
 
 @property (nonatomic)                NSArray *bestScores;
 
+@property (weak, nonatomic) IBOutlet UIImageView *imgBackground;
+
 @property (weak, nonatomic) IBOutlet UILabel *lblPlayer1;
 @property (weak, nonatomic) IBOutlet UILabel *lblPlayer2;
 @property (weak, nonatomic) IBOutlet UILabel *lblPlayer3;
 @property (weak, nonatomic) IBOutlet UILabel *lblPlayer4;
 @property (weak, nonatomic) IBOutlet UILabel *lblPlayer5;
 @property (weak, nonatomic) IBOutlet UILabel *lblPlayer6;
+@property (weak, nonatomic) IBOutlet UILabel *lblPlayer7;
+@property (weak, nonatomic) IBOutlet UILabel *lblPlayer8;
+@property (weak, nonatomic) IBOutlet UILabel *lblPlayer9;
+@property (weak, nonatomic) IBOutlet UILabel *lblPlayer10;
+@property (weak, nonatomic) IBOutlet UILabel *lblPlayer11;
+@property (weak, nonatomic) IBOutlet UILabel *lblPlayer12;
 
 @property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer1;
 @property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer2;
@@ -26,6 +34,12 @@
 @property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer4;
 @property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer5;
 @property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer6;
+@property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer7;
+@property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer8;
+@property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer9;
+@property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer10;
+@property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer11;
+@property (weak, nonatomic) IBOutlet UILabel *lblScorePlayer12;
 
 - (IBAction)returnAction:(id)sender;
 
