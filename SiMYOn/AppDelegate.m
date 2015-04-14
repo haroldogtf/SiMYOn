72c42dbef02cb0d9d652f18d81a4c1bf2b95c03b
Haroldo Gondim <hgtf@cin.ufpe.br>
2015-04-14 18:39:23 -0300

    offline ranking

--- SiMYOn/AppDelegate.m
+++ SiMYOn/AppDelegate.m
@@ -69,10 +69,14 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
 - (void) configureRanking {
     for (int i = 1; i <= OFFLINE_RANKING; i++) {
-        [Util setString:INITAL_NAME   forKey:[PLAYER       stringByAppendingFormat:@"%d", i]];
-        [Util setString:INITIAL_SCORE forKey:[SCORE_PLAYER stringByAppendingFormat:@"%d", i]];
+        
+        NSString *player = [PLAYER stringByAppendingFormat:@"%d", i];
+        
+        if(![[NSUserDefaults standardUserDefaults] objectForKey:player]) {
+            [Util setString:INITAL_NAME   forKey:player];
+            [Util setString:INITIAL_SCORE forKey:[SCORE_PLAYER stringByAppendingFormat:@"%d", i]];
+        }
     }
-    
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
